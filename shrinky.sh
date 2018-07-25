b99da3bf4a2800d22adcd424c5de2e0e75698ee4
Alexander F Rødseth <rodseth@gmail.com>
2018-07-25 08:37:07 -0700

    Update wrapper script

--- shrinky.sh
+++ shrinky.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 SHRINKY_MODULE_PATH=shrinky
-if [ ! -d "$SHRINKY_MODULE_PATH" ]; then
-  SHRINKY_MODULE_PATH=/usr/share/shrinky
-fi
 if [ -z $1 ]; then
-  echo 'Shrinky 0.1 (dnload fork)'
+  echo 'Shrinky 1.0'
   echo
   echo 'Provide the path to a C++ source file as the first argument.'
   echo
@@ -13,14 +10,14 @@ if [ -z $1 ]; then
   echo
   exit 1
 fi
-FILENAME="${1:-src/intro.cpp}"
+FILENAME="$1"
 shift
 SRCDIR="$(dirname $FILENAME)"
 [ -f "$SRCDIR/shrinky.h" ] || touch "$SRCDIR/shrinky.h"
 if [ -d "/usr/lib/arm-linux-gnueabihf/mali-egl" ]; then # Mali
-  /usr/bin/python3 -m "$SHRINKY_MODULE_PATH" -v "$FILENAME" --rpath "/usr/local/lib" -lc -ldl -lgcc -lm -lEGL -lGLESv2 -lSDL2 -m dlfcn "$@"
+  python -m shrinky -v "$FILENAME" --rpath "/usr/local/lib" -lc -ldl -lgcc -lm -lEGL -lGLESv2 -lSDL2 -m dlfcn "$@"
 else
-  /usr/bin/python3 -m "$SHRINKY_MODULE_PATH" -v "$FILENAME" "$@"
+  python -m shrinky -v "$FILENAME" "$@"
 fi
 if [ $? -ne 0 ]; then
   echo "${0}: compilation failed"
