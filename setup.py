61b977562ded27409f6e24449d659119b7d69bf4
Alexander F Rødseth <rodseth@gmail.com>
2018-07-25 07:11:11 -0700

    Add setup.py installation script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(name='Shrinky',
+      version='1.0',
+      description='Build small executables (dnload fork, for Arch Linux)', # dnload is here: https://github.com/faemiyah/dnload
+      url='https://github.com/xyproto/shrinky',
+      author='Alexander F. Rødseth',
+      author_email='xyproto@archlinux.org',
+      license="BSD",
+      py_modules=["shrinky"],
+      entry_points={
+          "console_scripts": [
+              "setconf = setconf:main",
+          ]
+      },
+      classifiers=[
+          "Environment :: Console",
+          "License :: OSI Approved :: BSD License", # New BSD license
+          "Programming Language :: Python",
+          "Topic :: System :: Shells",
+          "Topic :: Utilities",
+      ]
+      )
